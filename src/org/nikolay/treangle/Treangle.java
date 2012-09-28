79000c2f8c9413b14742d047648bd45eabb33335
Nikolay Dagil <Nikolay.Dagil@gmail.com>
2012-09-28 17:58:01 +0300

    Создал модель треугольника

--- src/org/nikolay/treangle/Treangle.java
+++ src/org/nikolay/treangle/Treangle.java
@@ -2,4 +2,32 @@ package org.nikolay.treangle;
 
 public class Treangle {
 
+	private double a;
+	private double b;
+	private double c;
+
+	public void setA(double a) {
+		this.a = a;
+	}
+
+	public double getA() {
+		return a;
+	}
+
+	public void setB(double b) {
+		this.b = b;
+	}
+
+	public double getB() {
+		return b;
+	}
+
+	public void setC(double c) {
+		this.c = c;
+	}
+
+	public double getC() {
+		return c;
+	}
+
 }
