a84b6bdf6d5c89ae3571ca7847a3f769fb711e23
Nikolay Dagil <Nikolay.Dagil@gmail.com>
2012-09-28 19:04:34 +0300

    [Ошибка] Добавил проверку на треугольник

--- src/org/nikolay/main/Main.java
+++ src/org/nikolay/main/Main.java
@@ -1,5 +1,35 @@
 package org.nikolay.main;
 
+import org.nikolay.metod.Metod;
+import org.nikolay.treangle.Treangle;
+
 public class Main {
+
+	public static void main(String[] args) {
+
+		Treangle treangle = new Treangle();
+
+		treangle.setA(10);
+		treangle.setB(4);
+		treangle.setC(5);
+
+		Metod metod = new Metod();
+
+		double a = treangle.getA();
+		double b = treangle.getB();
+		double c = treangle.getC();
+
+		System.out.print("Проверка на треугольник: ");
+
+		if (metod.isCheckTreangl(a, b, c) == metod.NOT_TREANGLE) {
+			System.out.println("Не треугольник");
+		} else {
+			System.out.println("Треугольник");
+
+			System.out.print("Площадь: ");
+			System.out.println(metod.square(a, b, c));
+		}
+
+	}
 
 }
--- src/org/nikolay/metod/Metod.java
+++ src/org/nikolay/metod/Metod.java
@@ -2,9 +2,47 @@ package org.nikolay.metod;
 
 public class Metod {
 
+	public static final double IS_TREANGLE = 1;
+	public static final double NOT_TREANGLE = 0;
+
+	private static final boolean IS_NULL = true;
+	private static final boolean NOT_NULL = false;
+
 	public double square(double a, double b, double c) {
 		return 0.25 * Math.sqrt((a + b + c) * (b + c - a) * (a + c - b)
 				* (a + b - c));
+	}
+
+	/**
+	 * Данный метод проверяет является ли объект треугольником по трем известным
+	 * сторонам
+	 * 
+	 * @param a
+	 * @param b
+	 * @param c
+	 * @return треугольник / не треугольник
+	 */
+	public double isCheckTreangl(double a, double b, double c) {
+
+		if (!isCheckForNul(a) && !isCheckForNul(b) && !isCheckForNul(c)) {
+									
+			if ((c <= a + b) && (a<= b +  c)
+					&& (b<= a + c )) {
+				return IS_TREANGLE;
+			} else {
+				return NOT_TREANGLE;
+			}
+		} else {
+			return NOT_TREANGLE;
+		}
+	}
+
+	private boolean isCheckForNul(double a) {
+		if (a == 0) {
+			return IS_NULL;
+		} else {
+			return NOT_NULL;
+		}
 	}
 
 }
