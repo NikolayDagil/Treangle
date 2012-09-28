dad0f4287bc09bd106a42daa31e1a85656951cc4
Nikolay Dagil <Nikolay.Dagil@gmail.com>
2012-09-28 17:59:55 +0300

    Добавил метод расчета площади

--- src/org/nikolay/metod/Metod.java
+++ src/org/nikolay/metod/Metod.java
@@ -2,4 +2,9 @@ package org.nikolay.metod;
 
 public class Metod {
 
+	public double square(double a, double b, double c) {
+		return 0.25 * Math.sqrt((a + b + c) * (b + c - a) * (a + c - b)
+				* (a + b - c));
+	}
+
 }
